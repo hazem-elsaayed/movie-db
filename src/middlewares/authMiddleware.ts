be9c94b6ca7f060dcba707685ba0895c71427af8
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-11-21 21:01:48 +0200

    feat: add authentication middleware for token verification

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { CustomError } from '../utils/customError';
+import { AuthenticatedRequest } from '../utils/interfaces';
+
+export const authenticate = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    throw new CustomError('Authentication token is missing', 401);
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    throw new CustomError('Invalid authentication token', 401);
+  }
+};
