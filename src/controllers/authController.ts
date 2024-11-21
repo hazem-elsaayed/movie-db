385453121831d33d30d564ec7e54099bcc8efc51
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-11-21 15:07:05 +0200

    feat: implement authentication controller with register and login methods

---
+++ src/controllers/authController.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import { AuthService } from '../services/authService';
+import { CustomError } from '../utils/customError';
+
+const authService = new AuthService();
+
+export class AuthController {
+  public static async register(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { email, password, username } = req.body;
+      const user = await authService.register(email, password, username);
+      res.status(201).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public static async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email, password } = req.body;
+      const { user, token } = await authService.login(email, password);
+      res.status(200).json({ user, token });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
