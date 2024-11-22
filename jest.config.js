0a88db35dc8943d978ca3b0257a46a980853e2b3
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-11-22 21:12:14 +0200

    chore: update jest configuration for improved test handling and module resolution

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,21 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['/dist/'],
   coveragePathIgnorePatterns: [
-    "/node_modules/",
-    "/dist/",
-    "/src/config/",
-    "/src/models/",
-    "/src/seeders/",
-    "/src/validators/",
+    '/node_modules/',
+    '/dist/',
+    '/src/config/',
+    '/src/models/',
+    '/src/seeders/',
+    '/src/validators/',
   ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
